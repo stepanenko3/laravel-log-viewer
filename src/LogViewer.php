de06d0d07c88c47df730e32f2bbb46ab3d22b298
Artem Stepanenko <stpnnk.artem@gmail.com>
2022-08-25 22:42:41 +0300

    update

--- src/LogViewer.php
+++ src/LogViewer.php
@@ -3,27 +3,11 @@
 namespace Stepanenko3\LaravelLogViewer;
 
 use Illuminate\Support\Collection;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\File;
 use Stepanenko3\LaravelLogViewer\Helpers\LogFile;
 
 class LogViewer
 {
     public static ?Collection $_cachedFiles = null;
-
-    public function rglob($pattern)
-    {
-        $files = glob($pattern);
-
-        foreach (glob(dirname($pattern) . '/*', GLOB_ONLYDIR | GLOB_NOSORT) as $dir) {
-            $files = array_merge(
-                [],
-                ...[$files, $this->rglob($dir . "/" . basename($pattern))],
-            );
-        }
-
-        return $files;
-    }
 
     /**
      * @return Collection|LogFile[]
@@ -34,11 +18,11 @@ class LogViewer
             $files = [];
 
             foreach (config('log-viewer.include_files', []) as $pattern) {
-                $files = array_merge($files, $this->rglob(storage_path() . '/logs/' . $pattern));
+                $files = array_merge($files, rglob(storage_path() . '/logs/' . $pattern));
             }
 
             foreach (config('log-viewer.exclude_files', []) as $pattern) {
-                $files = array_diff($files, $this->rglob(storage_path() . '/logs/' . $pattern));
+                $files = array_diff($files, rglob(storage_path() . '/logs/' . $pattern));
             }
 
             $files = array_reverse($files);
